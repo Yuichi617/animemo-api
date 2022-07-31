9ee3fc739c8d33f162b86ad9d07ac0c5025453f3
Yuichi Sugiyama <ozwald176@gmail.com>
2022-07-31 12:47:47 +0900

    jestの設定

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    moduleFileExtensions: ['js', 'ts'], // テスト対象の拡張子を列挙する
+    moduleFileExtensions: ['js','ts','json'], // テスト対象の拡張子を列挙する
     preset: 'ts-jest', // A preset that is used as a base for Jest's configuration
     testEnvironment: 'node',  // The test environment that will be used for testing
     testMatch: [ // The glob patterns Jest uses to detect test files
